5909be024c5a31bf2f1c0f0b779b195459899bb8
Dieter Komendera <dieter@komendera.com>
2025-06-07 20:09:36 +0200

    Add initial project

---
+++ src/instadon/app.clj
@@ -0,0 +1,123 @@
+(ns instadon.app
+  (:require [babashka.fs :as fs]
+            [babashka.http-client :as http]
+            [cheshire.core :as json]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+
+            [libpython-clj2.require :refer [require-python]]
+            [libpython-clj2.python :refer [py. py.. py.-] :as py]))
+
+(require-python
+ :from "/opt/homebrew/Cellar/instaloader/4.14.1/libexec/lib/python3.13/site-packages"
+ 'instaloader)
+
+(def L (instaloader/Instaloader))
+(py. L load_session_from_file "kommen")
+(def post (py. instaloader/Post from_shortcode (py.- L context) "DJeNLlxxTHl"))
+
+(py.- post caption)
+(let [profile (py. instaloader/Profile from_username (py.- L context) "kulturneubau")
+      posts (py. profile get_posts)]
+  posts)
+
+(def json-headers {:accept "application/json"
+                   :content-type "application/json"})
+
+(def config
+  {:mastodon {:instance "https://neubau.social"
+              :access-token "czPVtqZUvT5RvxTE_KVygunCiupXxre-71SBIndYf_o"}})
+
+
+
+(defn create-mastodon-draft
+  "Create a draft post in Mastodon"
+  [status media-ids visibility]
+  (let [url (str (get-in config [:mastodon :instance]) "/api/v1/statuses")
+        params {:status status
+                :media_ids media-ids
+                :visibility visibility
+                #_#_:draft true}
+        headers {"Authorization" (str "Bearer " (get-in config [:mastodon :access-token]))}]
+    (http/post url {:headers (merge json-headers
+                                    headers)
+                    :body (json/encode params)})))
+
+(comment
+  (create-mastodon-draft "TEST" [114485373086948381] "public")
+  )
+
+(defn upload-media-to-mastodon
+  "Upload media to Mastodon"
+  [content description]
+  (let [url (str (get-in config [:mastodon :instance]) "/api/v2/media")
+        headers {"Authorization" (str "Bearer " (get-in config [:mastodon :access-token]))}]
+    (-> (http/post
+         url
+         {:headers headers
+          :multipart [{:name "file" :content content}
+                      {:name "description" :content description}]})
+        :body
+        (json/parse-string true)
+        :id)))
+
+
+(comment
+  (upload-media-to-mastodon (io/input-stream (.getBytes (:body r)))
+                            "TEST"))
+
+(defn url->file [url]
+  (let [tmpfile (fs/create-temp-file {:suffix ".jpeg"})
+        file (io/file (.toString tmpfile))]
+    (io/copy (-> url (http/get {:as :bytes}) :body)
+             file)
+    file))
+
+(defn download [url]
+  (-> (http/post "https://cobalt.uber.space/"
+                 {:headers json-headers
+                  :body (json/encode {:url url})})
+      :body
+      (json/parse-string true)))
+
+
+(defn process [result]
+  (case (:status result)
+    "picker"
+    (->> result
+         :picker
+         (filter #(= "photo" (:type %)))
+         (map (comp url->file :url)))
+    "tunnel"
+    [(url->file (:url result))]))
+
+
+
+(comment
+  (process (download "https://www.instagram.com/kulturneubau/p/DJeNLlxxTHl/"))
+
+  (download "https://www.instagram.com/p/DI3AcneMlZW/")
+
+  (io/input-stream (-> (download "https://www.instagram.com/kulturneubau/p/DJeNLlxxTHl/")
+                       :picker
+                       first
+                       :url
+                       (http/get {:as :bytes})
+                       :body))
+  (upload-media-to-mastodon
+   (let [tmpfile (fs/create-temp-file {:suffix ".jpeg"})
+         file (io/file (.toString tmpfile))]
+     (io/copy (-> (download "https://www.instagram.com/kulturneubau/p/DJeNLlxxTHl/")
+                  :picker
+                  first
+                  :url
+                  (http/get {:as :bytes})
+                  :body)
+              file)
+     file)
+
+
+   "TEST")
+
+
+  )
